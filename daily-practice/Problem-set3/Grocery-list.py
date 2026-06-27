26bca4eebc94787cb5a30eb6d2f4d6282253135d
UPcasual <un.jugador.casual1@gmail.com>
2026-06-27 02:44:55 -0400

    terminado, aprendiendo sobre listas y diccionarios
    uso de sorted(list)
    trabajar con diccionarios y sus valores

--- daily-practice/Problem-set3/Grocery-list.py
+++ daily-practice/Problem-set3/Grocery-list.py
@@ -1,32 +1,26 @@
 '''
 Suppose that you’re in the habit of making a list of items you need from the grocery store.
 
-In a file called grocery.py, implement a program that prompts the user for items, one per line, until the user inputs control-d 
-(which is a common way of ending one’s input to a program). Then output the user’s grocery list in all uppercase, sorted alphabetically by item, 
+In a file called grocery.py, implement a program that prompts the user for items, one per line, until the user inputs control-d
+(which is a common way of ending one’s input to a program). Then output the user’s grocery list in all uppercase, sorted alphabetically by item,
 prefixing each line with the number of times the user inputted that item. No need to pluralize the items. Treat the user’s input case-insensitively.
 '''
 
-lista = {} # para agregar valor y una cantidad se debe utilziar un diccionarios. 
+lista = {} # para agregar valor y una cantidad se debe utilziar un diccionarios.
 
 
 #def orden():
     #la lista debe ser ordenado y se le debe agregar un valor de veces que se escribre
-
-def main():
+try:
     while True:
-        try:
-            texto = str(input("ingresa la lista de la compra: "))
-            texto_upp = texto.upper() 
-            if texto_upp == lista
+        input_ =input(("ingresas un item"))
+        upper_input = input_.upper()
+        if upper_input not in lista:
+            lista[upper_input]= 1
+        elif upper_input  in lista:
+            lista[upper_input] += 1
 
-            else:
-                
-            for i in lista:
-                print(lista())
-            #ingresar texto a la lista 
-            print(texto_upp)
-        except EOFError:
-            break
-
-
-main()
+except EOFError:
+    lista_ordenada= sorted(lista)
+    for item in lista_ordenada:
+        print(lista[item], item)
